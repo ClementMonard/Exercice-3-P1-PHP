9edf8892b5e6b3dc326cfca1a8369abac790d619
ClementMonard <clement.monard@hotmail.fr>
2018-07-25 15:40:43 +0200

    Exercice 3

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 3</title>
+  </head>
+  <body>
+      <p>
+        <?php
+          $km = 1;
+          echo $km;
+          ?>
+      <p>
+        <?php
+        $km = 3;
+        echo $km;
+        ?>
+      </p>
+      <p>
+        <?php
+        $km = 125;
+        echo $km;
+        ?>
+      </p>
+  </body>
+</html>
